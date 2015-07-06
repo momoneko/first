189fb22536f27ad7225cec37a59a705271e38f4a
panioglovadim <panioglo.vadim@gmail.com>
2015-07-06 15:17:58 +0300

    hello with random

--- src/config/views.py
+++ src/config/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+import random
 
 def hello(request):
-    return HttpResponse("Hello world")
+    r = random.randint(0,100)
+    return HttpResponse("Hello {0}".format(r))
